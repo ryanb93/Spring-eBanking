555cc5ca731efb829977443dc53042acba25bdb7
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-10-23 14:21:31 +0100

    Used propper casing

--- Backend/src/main/java/rest/config/Routes.java
+++ Backend/src/main/java/rest/config/Routes.java
@@ -2,10 +2,10 @@ package rest.config;
 
 public class Routes {
     
-    public static final String api = "/api";
-    public static final String accounts = api + "/accounts";
-    public static final String customerAccount = api + accounts + "/{id}";
-    public static final String details = api + "/details";
-    public static final String app = api + "/apps";
+    public static final String API = "/api";
+    public static final String ACCOUNTS = API + "/accounts";
+    public static final String COUSTOMER_ACCOUNT = API + ACCOUNTS + "/{id}";
+    public static final String DETAILS = API + "/details";
+    public static final String APP = API + "/apps";
     
 }
