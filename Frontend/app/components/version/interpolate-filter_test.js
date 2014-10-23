d7a8601eb26159b76d0097ffa876a908f55d509c
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-10-23 22:32:11 +0100

    Refactor of myApp to eBankingApp as app name

--- Frontend/app/components/version/interpolate-filter_test.js
+++ Frontend/app/components/version/interpolate-filter_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-describe('myApp.version module', function() {
-  beforeEach(module('myApp.version'));
+describe('eBankingApp.version module', function() {
+  beforeEach(module('eBankingApp.version'));
 
   describe('interpolate filter', function() {
     beforeEach(module(function($provide) {
--- Frontend/app/view2/view2_test.js
+++ Frontend/app/view2/view2_test.js
@@ -1,8 +1,8 @@
 'use strict';
 
-describe('myApp.view2 module', function() {
+describe('eBankingApp.view2 module', function() {
 
-  beforeEach(module('myApp.view2'));
+  beforeEach(module('eBankingApp.view2'));
 
   describe('view2 controller', function(){
 
