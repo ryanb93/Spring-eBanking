c8934ec561e31f97f6e5a0fec20e42df0b7e6605
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-10-23 18:52:20 +0100

    Update to account controller for testing new framework

--- Backend/src/main/java/rest/controller/AccountsController.java
+++ Backend/src/main/java/rest/controller/AccountsController.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rest.controller;
 
-/**
- *
- * @author jorden
- */
+import core.domain.Account;
+import core.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import rest.config.Routes;
+
+@RestController
+@RequestMapping(Routes.ACCOUNTS)
 public class AccountsController {
+    
+    //@Autowired
+    private AccountService accountService;
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Account> getAllAccounts() {
+        return new ResponseEntity<Account>(HttpStatus.OK);
+    }
     
 }
