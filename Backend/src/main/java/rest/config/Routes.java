3dc4a1e419f5a75805fb48f3534c215f06c84222
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-10-23 21:16:20 +0100

    Dealt with Third Party Apps url

--- Backend/src/main/java/rest/config/Routes.java
+++ Backend/src/main/java/rest/config/Routes.java
@@ -2,10 +2,11 @@ package rest.config;
 
 public class Routes {
     
-    public static final String API = "/api";
+    public static final String API = "/api/customers/{customer_id}";
     public static final String ACCOUNTS = API + "/accounts";
     public static final String CUSTOMER_ACCOUNT = "/{account_id}";
     public static final String DETAILS = API + "/details";
     public static final String APP = API + "/apps";
+    public static final String APP_ID = "/{app_id}";
     
 }
