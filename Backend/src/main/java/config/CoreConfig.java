6572a9316c7e2f6e7c6da1272175b12bdc119831
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-10-23 18:52:45 +0100

    Some config magic

---
+++ Backend/src/main/java/config/CoreConfig.java
@@ -0,0 +1,17 @@
+package config;
+
+import core.repository.AccountsRepository;
+import core.services.AccountEventHandler;
+import core.services.AccountService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CoreConfig {
+   
+//  @Bean
+//  public AccountService createService(AccountsRepository repo) {
+//    return new AccountEventHandler(repo);
+//  }
+//    
+}
---
+++ Backend/src/main/java/config/MVCConfig.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"rest.controller"})
+public class MVCConfig {}
