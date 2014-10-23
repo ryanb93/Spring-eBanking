6af159c52a580a55b762c6318ef76bb3092bf49c
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-23 15:13:22 +0100

    Added customer domain

--- Backend/src/main/java/core/domain/Customer.java
+++ Backend/src/main/java/core/domain/Customer.java
@@ -1,5 +1,14 @@
 package core.domain;
 
+import java.util.Date;
+import java.util.List;
+
 public class Customer {
+    
+    private String firstName;
+    private String lastName;    
+    private Date dateOfBirth;
+    private Address address;
+    private List<Account> accounts;
     
 }
