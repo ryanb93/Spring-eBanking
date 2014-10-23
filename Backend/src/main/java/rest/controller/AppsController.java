50b8367d1c995948ee6210d66abbfb1601f14acd
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-10-23 21:16:20 +0100

    Dealt with Third Party Apps url

--- Backend/src/main/java/rest/controller/AppsController.java
+++ Backend/src/main/java/rest/controller/AppsController.java
@@ -1,11 +1,31 @@
 package rest.controller;
 
+import core.domain.ThirdPartyApp;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import rest.config.Routes;
 
 @RestController
 @RequestMapping(Routes.APP)
 public class AppsController {
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<ThirdPartyApp> getAllAccounts() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = Routes.APP_ID, method = RequestMethod.GET)
+    public ThirdPartyApp getThirdPartyApp(@PathVariable( "app_id" ) int id) {
+        return new ThirdPartyApp("All your monies", true, true);
+    }
+    
+    @RequestMapping(value = Routes.APP_ID, method = RequestMethod.PUT)
+    public ThirdPartyApp updateThirdPartyApp(@PathVariable( "app_id" ) int id) {
+        return new ThirdPartyApp("All your monies", true, true);
+    }
     
 }
