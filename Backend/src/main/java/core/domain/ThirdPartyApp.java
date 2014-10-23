8c111a720c14eb65289bb50fb6b8fcf75502b148
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-23 19:05:10 +0100

    Updated comments on ThirdPartyApp

--- Backend/src/main/java/core/domain/ThirdPartyApp.java
+++ Backend/src/main/java/core/domain/ThirdPartyApp.java
@@ -26,36 +26,64 @@ public class ThirdPartyApp {
         super();
         this.setName(name);
         this.applicationId = UUID.randomUUID();
+        this.setEnabled(true);
         this.read = read;
         this.write = write;
-        this.enabled = true;
     }
     
+    /**
+     * Sets the name
+     * @param name - The new name.
+     */
     public final void setName(String name) {
         if(name == null || name.equals("")) { throw new IllegalArgumentException("Name can not be empty."); }
         this.name = name;
     }
     
+    /**
+     * Sets the value of enabled.
+     * @param enabled - If the application is enabled or not.
+     */
     public final void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
     
+    /**
+     * Gets the application ID
+     * @return the application ID
+     */
     public UUID getApplicationId() {
         return this.applicationId;
     }
     
+    /**
+     * Gets the application name
+     * @return the application name
+     */
     public String getName() {
         return this.name;
     }
     
+    /**
+     * Gets the status of the read access.
+     * @return if the application has read access
+     */
     public boolean getRead() {
         return this.read;
     }
     
+    /**
+     * Gets the status of the write access.
+     * @return if the application has write access.
+     */
     public boolean getWrite() {
         return this.write;
     }
     
+    /**
+     * Gets the enabled status.
+     * @return if the application is enabled.
+     */
     public boolean getEnabled() {
         return this.enabled;
     }
