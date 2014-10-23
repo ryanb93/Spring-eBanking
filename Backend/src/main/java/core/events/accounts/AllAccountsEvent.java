23bed8391684cd60344388cd14f2f50f612b9c1e
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-10-23 18:48:52 +0100

    Implementations of basic events for Account domain

---
+++ Backend/src/main/java/core/events/accounts/AllAccountsEvent.java
@@ -0,0 +1,7 @@
+package core.events.accounts;
+
+import core.events.ReadEvent;
+
+public class AllAccountsEvent extends ReadEvent {
+    
+}
---
+++ Backend/src/main/java/core/events/accounts/RequestAllAccountsEvent.java
@@ -0,0 +1,9 @@
+package core.events.accounts;
+
+import core.events.RequestReadEvent;
+
+public class RequestAllAccountsEvent extends RequestReadEvent {
+    
+    
+    
+}
