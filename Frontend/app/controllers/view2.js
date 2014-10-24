bace70964b3bf6e25340796fd50dad19fede9664
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-10-24 15:15:56 +0100

    Split away structure. Independent dirs

--- Frontend/app/controllers/view2.js
+++ Frontend/app/controllers/view2.js
@@ -4,7 +4,7 @@ angular.module('eBankingApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
-    templateUrl: 'views/view2/view2.html',
+    templateUrl: 'views/view2.html',
     controller: 'View2Ctrl'
   });
 }])
