e7c7b92eb05e6498b768358a0b448f515d800668
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-10-24 13:26:18 +0100

    AccountsView skeleton

---
+++ Frontend/app/accountsView/accountsView_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('eBankingApp.accountsView module', function() {
+
+  beforeEach(module('eBankingApp.accountsView'));
+
+  describe('accountsView controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var accountsViewCtrl = $controller('AccountsViewCtrl');
+      expect(accountsViewCtrl).toBeDefined();
+    }));
+
+  });
+});
