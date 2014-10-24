bcd84fd3e5740ca7b40f9f3571e6ae597e8e58ef
Delicious Fish <ben.fletcher.uk@gmail.com>
2014-10-24 12:00:49 +0100

    Modified Transaction route and removed unused reference in AccountsController. Also added TransactionService and TransactionsController.

--- Backend/src/main/java/rest/controller/AccountsController.java
+++ Backend/src/main/java/rest/controller/AccountsController.java
@@ -6,7 +6,6 @@ import core.domain.Customer;
 import core.domain.PostalAddress;
 import core.services.AccountService;
 import java.util.Date;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
