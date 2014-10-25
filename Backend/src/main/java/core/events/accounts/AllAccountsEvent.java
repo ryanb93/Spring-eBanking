84be277e484b8129966c1380239ac35eb5bac941
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-26 00:56:29 +0100

    Updated AllAccountsEvent to contain a list of accounts.

--- Backend/src/main/java/core/events/accounts/AllAccountsEvent.java
+++ Backend/src/main/java/core/events/accounts/AllAccountsEvent.java
@@ -1,7 +1,20 @@
 package core.events.accounts;
 
 import core.events.ReadEvent;
+import core.domain.Account;
+import java.util.Collections;
+import java.util.List;
 
 public class AllAccountsEvent extends ReadEvent {
+
+    private final List<Account> accounts;
+
+    public AllAccountsEvent(List<Account> accounts) {
+      this.accounts = Collections.unmodifiableList(accounts);
+    }
+
+    public List<Account> getAccounts() {
+      return this.accounts;
+    }
     
 }
