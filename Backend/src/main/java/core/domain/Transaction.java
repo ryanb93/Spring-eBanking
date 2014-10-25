3fef7a1e0e083c2618ff4e7c234829dbbed2739c
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-10-25 16:44:59 +0100

    Formatted Domain classes code

--- Backend/src/main/java/core/domain/Transaction.java
+++ Backend/src/main/java/core/domain/Transaction.java
@@ -8,33 +8,38 @@ import org.springframework.data.mongodb.core.mapping.Document;
 /**
  * A class which encapsulates data for a Transaction.
  */
-@Document(collection="transactions")
+@Document(collection = "transactions")
 public class Transaction {
+
     @Id
     private String transactionId;   // A unique transaction ID
+
     @NotNull
     private String accountId;       // The unique ID of either the sender or recipient account
+
     @NotNull
-    private String sender;        // The UUID sender in the transaction
+    private String sender;          // The UUID sender in the transaction
+
     @NotNull
-    private String recipient;     // The recipient of the transaction
+    private String recipient;       // The recipient of the transaction
+
     @NotNull
-    private Double value;         // The value of the 
+    private Double value;           // The value of the 
+
     @NotNull
-    private Date date;            // The date the transaction was commited
-    
-    public Transaction() {}
-    
-     /**
+    private Date date;              // The date the transaction was commited
+
+    /**
      * Creates a new Transaction object.
-     * 
+     *
      * @param sender - The UUID of the sender.
      * @param recipient - The last name of the customer.
      * @param value - The customer's data of birth.
      * @param date - The address of the customer.
      */
-    public Transaction(String sender, String recipient, Double value, Date date){
+    public Transaction(String sender, String recipient, Double value, Date date) {
         super();
+
         // Set values using setters.
         this.setSender(sender);
         this.setRecipient(recipient);
@@ -44,42 +49,59 @@ public class Transaction {
 
     /**
      * Sets the name of the sender.
+     *
      * @param sender - The name of the sender.
      */
     public final void setSender(String sender) {
-        if(sender == null || sender.equals("")){throw new IllegalArgumentException("This sender is either null or invalid.");}
+        if (sender == null || sender.equals("")) {
+            throw new IllegalArgumentException("This sender is either null or invalid.");
+        }
+        
         this.sender = sender;
     }
 
     /**
      * Sets the first name of the customer.
+     *
      * @param recipient - The first name of the recipient.
      */
     public final void setRecipient(String recipient) {
-        if(recipient == null || recipient.equals("")){throw new IllegalArgumentException("This recipient is either null or invalid.");}
+        if (recipient == null || recipient.equals("")) {
+            throw new IllegalArgumentException("This recipient is either null or invalid.");
+        }
+        
         this.recipient = recipient;
     }
 
-     /**
+    /**
      * Sets the value of the transaction.
+     *
      * @param value - The value of the transaction.
      */
     public final void setValue(Double value) {
-        if(value == null){throw new IllegalArgumentException("This value is invalid.");}
+        if (value == null) {
+            throw new IllegalArgumentException("This value is invalid.");
+        }
+        
         this.value = value;
     }
 
     /**
      * Sets the date of the transaction.
+     *
      * @param date - The date of the transaction.
      */
     public final void setDate(Date date) {
-        if(date == null){throw new IllegalArgumentException("This date is invalid.");}
+        if (date == null) {
+            throw new IllegalArgumentException("This date is invalid.");
+        }
+        
         this.date = date;
     }
 
     /**
      * Gets the transactionId.
+     *
      * @return transactionId - The ID of the transaction.
      */
     public String getTransactionId() {
@@ -88,6 +110,7 @@ public class Transaction {
 
     /**
      * Gets the date of the transaction.
+     *
      * @return accountId - The id of the recipient or sender account.
      */
     public String getAccountId() {
@@ -96,6 +119,7 @@ public class Transaction {
 
     /**
      * Gets the sender in the transaction.
+     *
      * @return sender - The sender in the transaction.
      */
     public String getSender() {
@@ -104,6 +128,7 @@ public class Transaction {
 
     /**
      * Gets the date of the transaction.
+     *
      * @return recipient - the recipient in the transaction.
      */
     public String getRecipient() {
@@ -112,6 +137,7 @@ public class Transaction {
 
     /**
      * Gets the value of the transaction.
+     *
      * @return value - The value of the transaction.
      */
     public Double getValue() {
@@ -120,11 +146,11 @@ public class Transaction {
 
     /**
      * Sets the date of the transaction.
+     *
      * @return date - The date of the transaction.
      */
     public Date getDate() {
         return date;
     }
-    
-    
+
 }
