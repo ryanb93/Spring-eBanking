d5ef097203b859185e57d922648bb53315d00527
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-26 00:56:02 +0100

    Renamed AccountDetails to AccountDetailsEvent

--- Backend/src/main/java/core/events/accounts/AccountDetailsEvent.java
+++ Backend/src/main/java/core/events/accounts/AccountDetailsEvent.java
@@ -6,11 +6,11 @@ import core.domain.Account;
  * This is a class that is returned to a service when it has
  * requested the details of a single account.
  */
-public class AccountDetails {
+public class AccountDetailsEvent {
     
     private final Account account;
     
-    public AccountDetails(Account account) {
+    public AccountDetailsEvent(Account account) {
         this.account = account;
     }
     
