21d77281385c81557493c4d85c17445ca9f6db17
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-25 16:11:13 +0100

    Moved hardcoded routes into Routes file

--- Backend/src/main/java/rest/config/Routes.java
+++ Backend/src/main/java/rest/config/Routes.java
@@ -6,10 +6,18 @@ public class Routes {
     public static final String ACCOUNTS = API + "/accounts";
     public static final String CUSTOMER_ACCOUNT = "/{account_id}";
     public static final String DETAILS = API + "/details";
+    
     public static final String APP = API + "/apps";
     public static final String APP_ID = "/{app_id}";
+    public static final String SINGLE_APP = "/singleApp";
+
     public static final String TRANSACTIONS = ACCOUNTS + CUSTOMER_ACCOUNT + "/transactions";
     
+    /**
+     * Test Routes - Not for production
+     */
+    public static final String JSON_ACCOUNT = "/jsonAccount";
     public static final String CUSTOMER_ADMIN = "/api/customers";
+    public static final String SINGLE_CUSTOMER = "/api/customers/{id}";
     
 }
