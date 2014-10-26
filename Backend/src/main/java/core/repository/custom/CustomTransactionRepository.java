19f27f3ad6e73fc2435e6e1df320459b18ca9ee1
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-26 12:53:03 +0000

    Removed the idea of a custom repository and moved to using abstract class instead.
    - It seems unnecessary to put our custom requests in its own class. Instead we can just put it within our normal Repositories and then make the implementation abstract so that Spring can still implement the methods that are missing using Autowired.

--- Backend/src/main/java/core/repository/custom/CustomTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package core.repository.custom;
-
-import core.domain.Transaction;
-import java.util.List;
-
-
-public interface CustomTransactionRepository {
-    public List<Transaction> findAllByAccountId(String accountId);
-}
