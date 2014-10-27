1e2b94d2b5aea3bc902116fbe4bf1fd431181ddc
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-27 10:37:52 +0000

    Added ability to get a single third party application back

--- Backend/src/main/java/core/services/eventHandlers/ThirdPartyAppsEventHandler.java
+++ Backend/src/main/java/core/services/eventHandlers/ThirdPartyAppsEventHandler.java
@@ -3,6 +3,8 @@ package core.services.eventHandlers;
 import core.domain.ThirdPartyApp;
 import core.events.thirdPartyApps.AllThirdPartyAppsEvent;
 import core.events.thirdPartyApps.RequestAllThirdPartyAppsEvent;
+import core.events.thirdPartyApps.RequestThirdPartyAppDetailsEvent;
+import core.events.thirdPartyApps.ThirdPartyAppDetailsEvent;
 import core.repository.ThirdPartyAppRepository;
 import core.services.ThirdPartyAppsService;
 import java.util.List;
@@ -18,5 +20,12 @@ public class ThirdPartyAppsEventHandler implements ThirdPartyAppsService {
         String customerId = requestAllThirdPartyAppsEvent.getCustomerId();
         List<ThirdPartyApp> apps = thirdPartyAppsRepository.findAllByCustomerId(customerId);
         return new AllThirdPartyAppsEvent(apps);
+    }
+    
+    @Override
+    public ThirdPartyAppDetailsEvent requestThirdPartyAppDetails(RequestThirdPartyAppDetailsEvent requestThirdPartyAppDetailsEvent) {
+        String appId = requestThirdPartyAppDetailsEvent.getAppId();
+        ThirdPartyApp app = thirdPartyAppsRepository.findOne(appId);
+        return new ThirdPartyAppDetailsEvent(app);
     }
 }
