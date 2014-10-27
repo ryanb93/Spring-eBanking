389c108612f080cd8afdf687c8d0be20f1a5fc0c
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-27 09:31:17 +0000

    Created ThirdPartyAppRepository findAllByCustomerId method

--- Backend/src/main/java/core/repository/ThirdPartyAppRepository.java
+++ Backend/src/main/java/core/repository/ThirdPartyAppRepository.java
@@ -1,9 +1,11 @@
 package core.repository;
 
 import core.domain.ThirdPartyApp;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ThirdPartyAppRepository extends MongoRepository<ThirdPartyApp, String> {
+    public List<ThirdPartyApp> findAllByCustomerId(String customerId);
 }
