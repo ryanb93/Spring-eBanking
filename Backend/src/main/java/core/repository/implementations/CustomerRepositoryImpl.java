8854388491f851a5011ee68b08dd61d6f613a238
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-27 10:00:48 +0000

    Added Customer Repository Implementation

---
+++ Backend/src/main/java/core/repository/implementations/CustomerRepositoryImpl.java
@@ -0,0 +1,8 @@
+package core.repository.implementations;
+
+import core.repository.custom.CustomCustomerRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CustomerRepositoryImpl implements CustomCustomerRepository {
+}
