4398e2e141f2d9a47ed82378befb53df6b050936
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-28 18:28:52 +0000

    Added single account data from backend

--- Frontend/app/accounts/accounts.js
+++ Frontend/app/accounts/accounts.js
@@ -2,37 +2,52 @@
 
 angular.module('eBanking.accountControllers', [])
 
-    .config(['$routeProvider', function($routeProvider) {
-      $routeProvider.when('/accounts', {
-        templateUrl: 'accounts/accounts.html',
-      });
+.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.when('/accounts', {
+            templateUrl: 'accounts/accounts.html',
+        });
 
-      $routeProvider.when('/accounts/:accountNumber', {
-        templateUrl: 'accounts/singleAccount.html',
-      });
-    }])
+        $routeProvider.when('/accounts/:accountNumber', {
+            templateUrl: 'accounts/singleAccount.html',
+        });
+    }
+])
 
-    .controller('accountsController', ['$scope', 'eBankingAPIservice',
-        function($scope, eBankingAPIservice) {
+.controller('accountsController', ['$scope', 'eBankingAPIservice',
+    function($scope, eBankingAPIservice) {
 
-            console.log("Calling eBanking service");
+        $scope.accountsList = [];
 
-            $scope.accountsList = [];
-            
-            var accountIds = null;
+        var accountIds = null;
 
-            var customerId = "544be631036458271642f6bb";
-            
-            eBankingAPIservice.getAccounts(customerId).get(function(ids) {
-                accountIds = ids.accountIds;
-                angular.forEach(accountIds, function(accountId) {
-                    
-                    eBankingAPIservice.getAccount(customerId, accountId).get(function(details) {
-                        $scope.accountsList.push(details);
-                    });
+        var customerId = "544be631036458271642f6bb";
+
+        eBankingAPIservice.getAccounts(customerId).get(function(ids) {
+            accountIds = ids.accountIds;
+            angular.forEach(accountIds, function(accountId) {
+
+                eBankingAPIservice.getAccount(customerId, accountId).get(function(details) {
+                    $scope.accountsList.push(details);
                 });
-
             });
 
-        }
+        });
+
+    }
+])
+
+.controller('singleAccountController', ['$scope', '$routeParams', 'eBankingAPIservice',
+    function($scope, $routeParams, eBankingAPIservice) {
+
+        $scope.account = null;
+
+        var accountId = $routeParams.accountNumber;
+        var customerId = "544be631036458271642f6bb";
+
+        eBankingAPIservice.getAccount(customerId, accountId).get(function(account) {
+            $scope.account = account;
+        });
+
+    }
 ]);
