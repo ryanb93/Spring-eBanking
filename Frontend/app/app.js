b058e3c064fc870f887e3b81448d4df1ce6d15b9
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-28 23:43:05 +0000

    More frontend UI tweaks.
    - Removed footer, kept getting in the way of everything, also horrible on mobile.
    - Added capitalise filter so we can show the CAPITALISED ENUMS on the UI nicely.
    - All accounts page no longer shows the dollar sign because we use pounds
    - Created a nice box to show the current account balance.
    - Created a fake timeline. THIS WOULD BE AMAZING TO ACTUALLY IMPLEMENT.
    	- If we don't want it we can just chuck it out and put in a table of transactions, just think it makes it look like its actually doing something useful on the client side rather than just showing data from a database.

--- Frontend/app/app.js
+++ Frontend/app/app.js
@@ -10,4 +10,14 @@ angular.module('eBanking', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/accounts'});
-}]);
+}])
+.filter('capitalize', function() {
+  return function(input, scope) {
+    if (input!=null)
+    input = input.toLowerCase();
+    return input.substring(0,1).toUpperCase()+input.substring(1);
+  }
+});
+
+
+
