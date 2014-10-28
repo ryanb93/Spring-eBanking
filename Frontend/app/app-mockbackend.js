fd204a013759b0bcf2e2520c0d3647b5669c7731
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-28 12:00:34 +0000

    Formatted mockbackend JSON

--- Frontend/app/app-mockbackend.js
+++ Frontend/app/app-mockbackend.js
@@ -6,23 +6,32 @@
 
 
     var jsonCustomer = {
-                        "customerId": "544be631036458271642f6bb",
-                        "firstName": "Jorden",
-                        "lastName": "Whitefield",
-                        "dateOfBirth": 16523,
-                        "address": {
-                            "houseNumber": "15",
-                            "street": "Test Road",
-                            "city": "Testingville",
-                            "county": "Testers",
-                            "country": "England",
-                            "postalCode": "TE5 T1N"
-                        }
-                    };
+        "customerId": "544be631036458271642f6bb",
+        "firstName": "Jorden",
+        "lastName": "Whitefield",
+        "dateOfBirth": 16523,
+        "address": {
+            "houseNumber": "15",
+            "street": "Test Road",
+            "city": "Testingville",
+            "county": "Testers",
+            "country": "England",
+            "postalCode": "TE5 T1N"
+        }
+    };
 
-    var accounts = {"accountIds":["544cf4610364aaa77dd7132a", "544cf4610364aaa77dd7132b", "544cf4610364aaa77dd7132c"]};    
+    var accounts = {
+        "accountIds": ["544cf4610364aaa77dd7132a", "544cf4610364aaa77dd7132b", "544cf4610364aaa77dd7132c"]
+    };
 
-    var account = {"accountId":"544cf4610364aaa77dd7132b","customerId":"544be631036458271642f6bb","accountNumber":"12345678","sortCode":"123456","accountType":"CURRENT","balance":500.0};           
+    var account = {
+        "accountId": "544cf4610364aaa77dd7132b",
+        "customerId": "544be631036458271642f6bb",
+        "accountNumber": "12345678",
+        "sortCode": "123456",
+        "accountType": "CURRENT",
+        "balance": 500.0
+    };
 
 
     function setupStubs() {
