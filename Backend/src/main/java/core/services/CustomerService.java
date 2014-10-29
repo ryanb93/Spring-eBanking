ba63a9b5076570b372980b654fc88a62d1a681b6
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-29 17:44:08 +0000

    Added support on the Backend to update customer details.

--- Backend/src/main/java/core/services/CustomerService.java
+++ Backend/src/main/java/core/services/CustomerService.java
@@ -6,9 +6,12 @@ import core.events.customers.CustomerDetailsEvent;
 import core.events.customers.RequestAllCustomersEvent;
 import core.events.customers.RequestCustomerDetailsEvent;
 import core.events.customers.RequestNewCustomerEvent;
+import core.events.customers.RequestUpdateCustomerDetailsEvent;
+import core.events.customers.UpdateCustomerDetailsEvent;
 
 public interface CustomerService {    
     public CustomerDetailsEvent requestCustomerDetails(RequestCustomerDetailsEvent requestCustomerDetailsEvent);
     public AllCustomersEvent requestAllCustomers(RequestAllCustomersEvent requestAllCustomersEvent);
     public CreateCustomerEvent requestNewCustomer(RequestNewCustomerEvent requestNewCustomerEvent);
+    public UpdateCustomerDetailsEvent requestUpdateCustomer(RequestUpdateCustomerDetailsEvent requestUpdateCustomerDetailsEvent);
 }
