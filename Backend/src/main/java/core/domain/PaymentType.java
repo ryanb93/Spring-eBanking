1a9f3a12b9a5632a79d7ef106f37214cc92b16db
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-29 00:52:28 +0000

    Created PaymentType enum

---
+++ Backend/src/main/java/core/domain/PaymentType.java
@@ -0,0 +1,9 @@
+package core.domain;
+
+/**
+ * A class which encapsulates Enum types for Payments.
+ *
+ */
+public enum PaymentType {
+    CASH, DEBIT, CREDIT, BACS, DIRECT_DEBIT, STANDING_ORDER, PAYPAL
+}
