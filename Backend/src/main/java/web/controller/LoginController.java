6c289f690edeb4704aaf2016254770bed6db8e00
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-10-30 17:49:22 +0000

    Return a login page.

--- Backend/src/main/java/web/controller/LoginController.java
+++ Backend/src/main/java/web/controller/LoginController.java
@@ -2,20 +2,25 @@ package web.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
-@RequestMapping("/login")
 @Controller
+@RequestMapping("/login")
 public class LoginController {
     
     @RequestMapping(method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public String sayHello() {
-       return "hello";
+    public ModelAndView login(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        ModelAndView modelAndView = new ModelAndView("login", "model", null);
+        return modelAndView;
     }
+
     
 }
