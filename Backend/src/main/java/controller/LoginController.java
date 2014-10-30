0633277daf44b52cc2dc9c5c89ccfd5fe940a1c3
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-10-30 00:15:05 +0000

    Skeleton code for serving a JSP to login

---
+++ Backend/src/main/java/controller/LoginController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String createLoginForm() {
+        return "login";
+    }
+    
+    
+}
