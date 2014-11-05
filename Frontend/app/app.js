8bc2119d58570e3bc2905b87cd362985d40bdef5
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-11-05 23:40:34 +0000

    Removed HTML5 mode, was breaking app when refreshing. Tried to attach controller to index page.

--- Frontend/app/app.js
+++ Frontend/app/app.js
@@ -9,16 +9,29 @@ angular.module('eBanking', [
   'eBanking.accountControllers',
   'eBanking.detailsControllers',
   'eBanking.APIService'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/accounts'});
+])
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/access_token=:accessToken', {
+      template: '',
+      controller: function ($location, AccessToken) {
+        var hash = $location.path().substr(1);
+        AccessToken.setTokenFromString(hash);
+        $location.path('/');
+        $location.replace();
+      }
+    }).otherwise({redirectTo: '/accounts'});
 }])
 .config(function($httpProvider){
         $httpProvider.defaults.headers.post['Content-Type'] = 'application/json; charset=UTF-8'
     })
-.config(function($locationProvider) {
-  $locationProvider.html5Mode(true).hashPrefix('!');
-})
+.controller('indexController', ['$scope', 'Profile',
+    function($scope, Profile) {
+
+        $scope.me = Profile.get();
+        console.log($scope.me)
+
+    }
+])
 .filter('capitalize', function() {
   return function(input, scope) {
     if (input) {
