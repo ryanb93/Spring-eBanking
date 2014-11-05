d762b98961e5ef3c02b812ce2090572e947b0eef
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-11-05 19:24:53 +0000

    Enter made form submit twice. Removed

--- Backend/src/main/webapp/static/js/loginController.js
+++ Backend/src/main/webapp/static/js/loginController.js
@@ -1,15 +1,6 @@
 $(document).ready(function () {
 
 	/*
-		When ENTER key is pressed, this event triggers jQuery to click the login 
-		button. This then in turn calls the click event of the login button.
-	*/
-	$('input').on('keydown', function (event) {
-		if (event.keyCode === 13)
-			$('#login_button').click()
-	});
-
-	/*
 		When login is pressed the OAuth js is called and credentials from form sent. 
 		If there is no error then the window redirect the user to the desiered service. 
 		If there is an error present then a message is shown to inform the user. 
