ee45631e103f68ae5ea07aa86d8f6df250e5d210
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-12-08 23:03:54 +0000

    Front-end now uses HTTPS

--- Frontend/app/app.js
+++ Frontend/app/app.js
@@ -3,7 +3,6 @@
 // Declare app level module which depends on views, and components
 angular.module('eBanking', [
 	'ngRoute',
-	'ngMockE2E',
   'oauth',
   'ngStorage',
   'eBanking.accountControllers',
