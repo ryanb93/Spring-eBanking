abcf13f9dd382a43787deef7254200cb052fb47b
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-12-09 21:27:36 +0000

    Removed unused imports in UserConfiguration.

--- Backend/src/main/java/config/UserConfiguration.java
+++ Backend/src/main/java/config/UserConfiguration.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import web.repository.UserRepository;
 import web.services.UserService;
 import web.services.eventHandlers.UserServiceEventHandler;
