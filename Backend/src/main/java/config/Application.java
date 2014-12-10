bc2475f08904bb40e1a94b09b20c251c1e0133d5
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-12-10 19:19:39 +0000

    Args not passing at runtime

--- Backend/src/main/java/config/Application.java
+++ Backend/src/main/java/config/Application.java
@@ -29,7 +29,7 @@ public class Application {
      * @param args - Command line arguments.
      */
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
     }
 
     @Bean
