30bafd93460ece5168096f496bc0ca07dc0fc6fe
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-12-10 20:39:19 +0000

    Commented components package.

--- Backend/src/main/java/components/CORSFilter.java
+++ Backend/src/main/java/components/CORSFilter.java
@@ -10,25 +10,54 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 
+/**
+ * This filter lets applications from different origins access our REST API.
+ * 
+ * Cross-Origin Resource Sharing (CORS) is a technique for relaxing
+ * the same-origin policy, allowing JavaScript on a web page to consume
+ * a REST API served from a different origin. (Source: http://spring.io/understanding/CORS)
+ */
 @Component
 public class CORSFilter implements Filter {
 
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Authorization, Content-Type, Origin, Cookie, X-Requested-With, Accept");
-        chain.doFilter(req, res);
-    }
+        /**
+         * Called each time a request is passed up through the filter chain.
+         * This is where we implement our filtering logic for CORS.
+         *
+         * @param req - The request from the client.
+         * @param res - The response from the server.
+         * @param chain - The filter chain object.
+         * @throws IOException
+         * @throws ServletException 
+         */
+        @Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+            //Cast the response.
+            HttpServletResponse response = (HttpServletResponse) res;
+            //Add headers to the response to allow CORS.
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods",  "GET, POST, PUT, DELETE, OPTIONS"); 
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Authorization, Content-Type, Origin, Cookie, X-Requested-With, Accept");
+            //Pass the request and response up the filter chain.
+            chain.doFilter(req, res);
+	}
 
-    @Override
-    public void init(FilterConfig filterConfig) {
-    }
+        /**
+         * Init method needed by interface.
+         * @param filterConfig 
+         */
+        @Override
+	public void init(FilterConfig filterConfig) {
+            //Do nothing.
+        }
 
-    @Override
-    public void destroy() {
-    }
+        /**
+         * Destroy method needed by interface.
+         */
+        @Override
+	public void destroy() {
+            //Do nothing.
+        }
 
 }
--- Backend/src/main/java/components/OAuthRestEntryPoint.java
+++ Backend/src/main/java/components/OAuthRestEntryPoint.java
@@ -8,9 +8,24 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+/**
+ * An AuthenticationEntryPoint implementation which allows us to define
+ * a custom error message when an authorization attempt fails.
+ * 
+ * Used by ExceptionTranslationFilter to commence an authentication scheme.
+ */
 @Component
 public class OAuthRestEntryPoint implements AuthenticationEntryPoint {
 
+    /**
+     * Commences the authentication scheme to add our custom error message.
+     * 
+     * @param request -  that resulted in an AuthenticationException
+     * @param response - so that the user agent can begin authentication
+     * @param authException - that caused the invocation
+     * @throws IOException
+     * @throws ServletException 
+     */
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Bad Credentials");
