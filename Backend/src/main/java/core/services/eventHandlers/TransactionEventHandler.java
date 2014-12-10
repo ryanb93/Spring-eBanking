2358546ff077c834dc604089b22d9a0c805e6950
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-12-10 19:27:53 +0000

    Commented TransactionEventHandler

--- Backend/src/main/java/core/services/eventHandlers/TransactionEventHandler.java
+++ Backend/src/main/java/core/services/eventHandlers/TransactionEventHandler.java
@@ -11,7 +11,6 @@ import core.events.transactions.RequestAllTransactionsEvent;
 import core.events.transactions.RequestCreateTransactionEvent;
 import core.events.transactions.RequestTransactionDetailsEvent;
 import core.events.transactions.TransactionDetailsEvent;
-import core.repository.AccountRepository;
 import core.repository.TransactionRepository;
 import core.services.AccountService;
 import core.services.TransactionService;
@@ -23,16 +22,29 @@ public class TransactionEventHandler implements TransactionService {
     private TransactionRepository transactionRepository;
 
     @Autowired
-    private AccountRepository accountRepository;
-
-    @Autowired
     private AccountService accountService;
 
+    /**
+     * Method which returns the details of a single transaction based on its database ID.
+     * 
+     * @param requestTransactionDetailsEvent - Event containing transaction ID.
+     * @return The transaction object containing details.
+     */
     @Override
     public TransactionDetailsEvent requestTransactionDetails(RequestTransactionDetailsEvent requestTransactionDetailsEvent) {
-        return new TransactionDetailsEvent(transactionRepository.findOne(requestTransactionDetailsEvent.getTransactionId()));
+        //Extract the transaction ID from the request event.
+        String transactionId = requestTransactionDetailsEvent.getTransactionId();
+        //Get the transaction from the repository.
+        Transaction transaction = transactionRepository.findOne(transactionId);
+        //Return the transaction details event.
+        return new TransactionDetailsEvent(transaction);               
     }
-
+    
+    /**
+     * 
+     * @param requestAllTransactionsEvent
+     * @return 
+     */
     @Override
     public AllTransactionsEvent requestAllTransactions(RequestAllTransactionsEvent requestAllTransactionsEvent) {
         String accountNumber = requestAllTransactionsEvent.getAccountNumber();
@@ -40,19 +52,18 @@ public class TransactionEventHandler implements TransactionService {
         return new AllTransactionsEvent(transactionRepository.findAllByAccountNumber(accountNumber, page));
     }
 
+    /**
+     * 
+     * @param createTransactionEvent
+     * @return 
+     */
     @Override
     public CreateTransactionEvent requestNewTransaction(RequestCreateTransactionEvent createTransactionEvent) {
-
         Transaction newTransaction = createTransactionEvent.getTransaction();
-
         String accountNumber = newTransaction.getAccountNumber();
-
         RequestAccountDetailsFromNumberEvent event = new RequestAccountDetailsFromNumberEvent(accountNumber);
-
-        AccountDetailsEvent accountEvent = accountService.requestAccountDetailsFromNumber(event);
-
+        AccountDetailsEvent accountEvent = accountService.requestAccountDetailsFromNumber(event);        
         Account account = accountEvent.getAccount();
-
         UpdateAccountBalanceEvent balanceEvent = new UpdateAccountBalanceEvent(account, newTransaction.getValue());
         accountService.updateAccountBalance(balanceEvent);
         transactionRepository.save(newTransaction);
