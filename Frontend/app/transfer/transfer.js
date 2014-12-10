2013050a457daee12ade751325b8a5a404ca48f5
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-12-10 16:12:06 +0000

    Updated Transfer page.

--- Frontend/app/transfer/transfer.js
+++ Frontend/app/transfer/transfer.js
@@ -13,6 +13,10 @@ angular.module('eBanking.transferControllers', [])
 .controller('transferController', ['$scope', 'eBankingAPIservice',
     function($scope, eBankingAPIservice) {
 
+        eBankingAPIservice.getAccounts().query(function(ids) {
+            $scope.accountsList = ids;
+        });
+
         $scope.processForm = function() {
             var test = eBankingAPIservice.postCustomerDetails().save($scope.details, function(result) {
                 console.log(result);
