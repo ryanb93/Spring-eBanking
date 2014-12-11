2ed2eb23bcb7d8afc4148696f525b811b169a0b7
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-12-11 02:50:42 +0000

    Details now show messages.

--- Frontend/app/details/details.js
+++ Frontend/app/details/details.js
@@ -17,11 +17,26 @@ angular.module('eBanking.detailsControllers', [])
             $scope.details = details;
         });
 
+        $scope.messages = {};
+        $scope.messages.details = {};
+        $scope.messages.details.err = {};
+        $scope.messages.details.success = {};
+        $scope.messages.details.err.err = false;
+        $scope.messages.details.success.success = false;
+
         $scope.processForm = function() {
             var test = eBankingAPIservice.postCustomerDetails().save($scope.details, function(result) {
-                console.log(result);
+                $scope.messages.details.success.success = true;
+                $scope.messages.details.err.err = false;
+                $scope.messages.details.success.message = "Thank you, your details have been updated successfully."; 
+            }, 
+            function(data, status, headers, config) {
+                $scope.messages.details.err.err = true;
+                $scope.messages.details.success.success = false;
+                $scope.messages.details.err.stat = data.status;
+                $scope.messages.details.err.message = data.statusText;
             });
-        }
 
+        };
     },
 ]);
