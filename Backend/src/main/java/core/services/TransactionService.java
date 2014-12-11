4dacc1bbcccf9f67afdf3e514633a431378e3e94
Delicious Fish <ben.fletcher.uk@gmail.com>
2014-12-11 14:48:54 +0000

    Commented AdminController and TransactionService.

--- Backend/src/main/java/core/services/TransactionService.java
+++ Backend/src/main/java/core/services/TransactionService.java
@@ -10,95 +10,112 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
- * 
+ * This is the TransactionService, which holds methods that may access
+ * the MongoDB Database
+ *  This allows for modularisation of the codebase
  */
 public class TransactionService implements TransactionServiceInterface {
-    
-    /** */
+
+    /**
+     * The Transaction Repository, required to save and retrieve Transactions
+     */
     @Autowired
     private TransactionRepository transactionRepository;
-    
-    /** */
+
+    /**
+     * The Account Service, required to call methods on Accounts      
+     */
     @Autowired
     private AccountServiceInterface accountService;
 
     /**
-     * Method which returns the details of a single transaction based on its database ID.
-     * 
+     * Method which returns the details of a single transaction based on its
+     * database ID.
+     *
      * @param requestTransactionDetailsEvent - Event containing transaction ID.
      * @return The transaction object containing details.
      */
     @Override
     public Transaction requestTransactionDetails(String transactionId) {
         //Return the transaction details event.
-        return transactionRepository.findOne(transactionId);               
+        return transactionRepository.findOne(transactionId);
     }
-    
+
     /**
-     * Method which returns the details of a all transactions for a given account.
-     * 
-     * @param requestAllTransactionsEvent - Event containing the request for all transactions of an account.
+     * Method which returns the details of a all transactions for a given
+     * account.
+     *
+     * @param requestAllTransactionsEvent - Event containing the request for all
+     * transactions of an account.
      * @return The transaction object containing all transaction details.
      */
     @Override
     public List<Transaction> requestAllTransactions(String accountNumber, int page) {
         return transactionRepository.findAllByAccountNumber(accountNumber, page);
     }
-    
-    /**
-     * Method which removes a Transaction from MongoDB.
-     * 
-     * @param transactionId - The ID of the transaction we want to remove..
-     */
-     @Override
-     public void removeTransaction(String transactionId) {
-        transactionRepository.delete(transactionRepository.findOne(transactionId));
-     }
-     
-     public List<Transaction> fetchAllMongoDbTransactions(){
-         return transactionRepository.findAll();
-     }
 
     /**
-     * Method which creates a new transaction, adds it to the repository and updates the 
-     * account which it is linked to with a new value.
-     * 
+     * Method which removes a Transaction from MongoDB.
+     *
+     * @param transactionId - The ID of the transaction we want to remove..
+     */
+    @Override
+    public void removeTransaction(String transactionId) {
+        transactionRepository.delete(transactionRepository.findOne(transactionId));
+    }
+
+    /**
+     * Method which returns all Transactions from MongoDB.
+     *
+     * @return A List of all transactions stored in MongoDb
+     */
+    public List<Transaction> fetchAllMongoDbTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    /**
+     * Method which creates a new transaction, adds it to the repository and
+     * updates the account which it is linked to with a new value.
+     *
      * @param createTransactionEvent - The transaction request.
      * @return Event object containing the newly created transaction.
      */
     @Override
     public Transaction requestNewTransaction(Transaction transaction) {
-        
+
         String senderAccountNumber = transaction.getSenderAccountNumber();
         String recipientAccountNumber = transaction.getRecipientAccountNumber();
         
+        // Pull the Sender and Recipient Account from MongoDb. If it does not exist, return null.
         Account senderAccount = accountService.requestAccountDetailsFromNumber(senderAccountNumber);
         Account recipientAccount = accountService.requestAccountDetailsFromNumber(recipientAccountNumber);
+        
+        // If the senderAccount is not null, update the senderAccount balance and assign it a transaction
+        if (senderAccount != null) {
 
-         if (senderAccount != null){
             accountService.updateAccountBalance(senderAccountNumber, -transaction.getValue());
             transaction.setAccountNumber(senderAccount.getAccountNumber());
-            if (recipientAccount != null){
-                if (transaction.getTransactionType() == null){
-                transaction.setTransactionType(TransactionType.BACS);
+            if (recipientAccount != null) {
+                if (transaction.getTransactionType() == null) {
+                    transaction.setTransactionType(TransactionType.BACS);
                 }
             }
             transactionRepository.save(transaction);
         }
-         
-        if (recipientAccount != null){
+        // If the recipientAccount is not null, update the recipientAccount balance and assign it a transaction
+        if (recipientAccount != null) {
             accountService.updateAccountBalance(recipientAccountNumber, transaction.getValue());
-            
+
             Transaction recipientTransaction = transaction;
-             if (recipientTransaction.getTransactionType() == null){
-             recipientTransaction.setTransactionType(TransactionType.BACS);
-             }
-            recipientTransaction.clearTransactionId();
-            recipientTransaction.setAccountNumber(recipientAccount.getAccountNumber());
-            if (senderAccount != null){
+            if (recipientTransaction.getTransactionType() == null) {
                 recipientTransaction.setTransactionType(TransactionType.BACS);
             }
-            transactionRepository.save(recipientTransaction);    
+            recipientTransaction.clearTransactionId();
+            recipientTransaction.setAccountNumber(recipientAccount.getAccountNumber());
+            if (senderAccount != null) {
+                recipientTransaction.setTransactionType(TransactionType.BACS);
+            }
+            transactionRepository.save(recipientTransaction);
         }
         return transactionRepository.findOne(transaction.getTransactionId());
 
--- Backend/src/main/java/web/controller/AdminController.java
+++ Backend/src/main/java/web/controller/AdminController.java
@@ -34,33 +34,45 @@ import web.repository.UserRepository;
 import web.services.interfaces.UserServiceInterface;
 
 /**
- * 
+ *
  * This class is for administration control, DO NOT USE IN PRODUCTION.
  *
- * This tool is purely built instead of providing you with a build script.
- * Here you can manage everything without the security limitations of the
- * OAuth API. The Admin controller runs on Backend web interface directly
- * and mimics the power that a real bank would have including creating new
- * customers and web users, adding money to accounts and deleting powers.
+ * This tool is purely built instead of providing you with a build script. Here
+ * you can manage everything without the security limitations of the OAuth API.
+ * The Admin controller runs on Backend web interface directly and mimics the
+ * power that a real bank would have including creating new customers and web
+ * users, adding money to accounts and deleting powers.
  */
 @RestController
 public class AdminController {
 
+    /**
+     * The Customer Service, required to call methods on Customers  
+     */
     @Autowired
     private CustomerServiceInterface customerService;
 
+    /**
+     * The Transaction Service, required to call methods on Transactions  
+     */
     @Autowired
     private TransactionServiceInterface transactionService;
-    
+
+    /**
+     * The Account Service, required to call methods on Accounts  
+     */
     @Autowired
     private AccountService accountService;
 
+    /**
+     * The User Service, required to call methods on Users  
+     */
     @Autowired
     private UserServiceInterface userService;
 
     /**
-     * This method allows for the return of all Customers in the MongoDB
-     * as a List so they can by shown in a table on the Admin Panel.
+     * This method allows for the return of all Customers in the MongoDB as a
+     * List so they can by shown in a table on the Admin Panel.
      *
      * @return A List of all the Customers stored in MongoDB
      */
@@ -72,11 +84,13 @@ public class AdminController {
     }
 
     /**
-     *This method allows for the return of all Customers, Accounts, Transactions,
-     * and Users stored in MongoDB as Lists so they can by shown in tables on the Admin Panel.
-     * 
+     * This method allows for the return of all Customers, Accounts,
+     * Transactions, and Users stored in MongoDB as Lists so they can by shown
+     * in tables on the Admin Panel.
+     *
      * @param model
-     * @return a modelandview with Lists of Customers, Accounts, Users, and Transactions.
+     * @return a modelandview with Lists of Customers, Accounts, Users, and
+     * Transactions.
      */
     @RequestMapping(Routes.ADMIN_PANEL)
     public ModelAndView showAdminPanel(ModelMap model) {
@@ -89,11 +103,12 @@ public class AdminController {
     }
 
     /**
-     *This method allows us to add a User to MongoDB from the Admin Panel.
-     * 
+     * This method allows us to add a User to MongoDB from the Admin Panel.
+     *
      * @param email the User Email Address
      * @param password the User Password
-     * @return the AdminPanel ModelAndView via a redirect after saving the new user.
+     * @return the AdminPanel ModelAndView via a redirect after saving the new
+     * user.
      */
     @RequestMapping(Routes.ADD_USER)
     public ModelAndView addUser(@RequestParam("email") String email, @RequestParam("password") String password) {
@@ -115,9 +130,12 @@ public class AdminController {
      * @param country the Customer Country
      * @param postCode the Customer PostCode
      * @param apiUserId the APIUser ID that we want to link this Customer to.
-     * @param builder a Spring Framework component to map Uri Components to String Values
-     * @return the AdminPanel ModelAndView via a redirect after saving the new Customer.
-     * @throws ParseException when the date of birth is entered in an incorrect format
+     * @param builder a Spring Framework component to map Uri Components to
+     * String Values
+     * @return the AdminPanel ModelAndView via a redirect after saving the new
+     * Customer.
+     * @throws ParseException when the date of birth is entered in an incorrect
+     * format
      */
     @RequestMapping(value = Routes.ADD_CUSTOMER, method = RequestMethod.POST)
     public ModelAndView createNewCustomer(@RequestParam("firstName") String firstName,
@@ -130,23 +148,27 @@ public class AdminController {
             @RequestParam("postCode") String postCode,
             @RequestParam("apiUserId") String apiUserId,
             UriComponentsBuilder builder) throws ParseException {
-
+        
+        // Build new Customer
         Customer customer = new Customer();
         customer.setFirstName(firstName);
         customer.setLastName(lastName);
         customer.setAddress(new PostalAddress(houseNumber, street, city, county, country, postCode));
         customer.setApiUserId(apiUserId);
-        
+
         customerService.requestNewCustomer(customer);
         return new ModelAndView("redirect:/adminPanel");
     }
 
     /**
-     *This method allows us to remove a Customer from MongoDB from the Admin Panel.
-     * 
+     * This method allows us to remove a Customer from MongoDB from the Admin
+     * Panel.
+     *
      * @param customerId the ID of the customer we want to remove
-     * @param builder a Spring Framework component to map Uri Components to String Values
-     * @return the AdminPanel ModelAndView via a redirect after removing the Customer.
+     * @param builder a Spring Framework component to map Uri Components to
+     * String Values
+     * @return the AdminPanel ModelAndView via a redirect after removing the
+     * Customer.
      */
     @RequestMapping(value = Routes.REMOVE_CUSTOMER, method = RequestMethod.POST)
     public ModelAndView removeCustomer(@RequestParam("selectedCustomerId") String customerId, UriComponentsBuilder builder) {
@@ -156,14 +178,17 @@ public class AdminController {
     }
 
     /**
-     *This method allows us to add an Account to MongoDB from the Admin Panel.
-     * 
+     * This method allows us to add an Account to MongoDB from the Admin Panel.
+     *
      * @param customerId the ID of the Customer we want to link the account to
      * @param selectedAccountType the Type of Account we want to create
      * @param sortCode the Sort Code of the account we want to create
-     * @param accountNumber - the Account Number of the Account we want to create
-     * @param builder a Spring Framework component to map Uri Components to String Values
-     * @return the AdminPanel ModelAndView via a redirect after saving the new Account.
+     * @param accountNumber - the Account Number of the Account we want to
+     * create
+     * @param builder a Spring Framework component to map Uri Components to
+     * String Values
+     * @return the AdminPanel ModelAndView via a redirect after saving the new
+     * Account.
      */
     @RequestMapping(value = Routes.ADD_ACCOUNT, method = RequestMethod.POST)
     public ModelAndView addAccount(@RequestParam("selectedCustomerId") String customerId,
@@ -173,9 +198,10 @@ public class AdminController {
             UriComponentsBuilder builder) {
 
         Account account = new Account();
-
+        // Set Account Type
         AccountType accountType = AccountType.valueOf(selectedAccountType.toUpperCase(Locale.US));
-
+        
+        // Build new Account
         account.setAccountType(accountType);
         account.setSortCode(sortCode);
         account.setAccountNumber(accountNumber);
@@ -187,11 +213,14 @@ public class AdminController {
     }
 
     /**
-     * This method allows us to remove an Account from MongoDB from the Admin Panel.
+     * This method allows us to remove an Account from MongoDB from the Admin
+     * Panel.
      *
      * @param accountId the ID of the Account we want to Remove
-     * @param builder a Spring Framework component to map Uri Components to String Values
-     * @return the AdminPanel ModelAndView via a redirect after removing the Account.
+     * @param builder a Spring Framework component to map Uri Components to
+     * String Values
+     * @return the AdminPanel ModelAndView via a redirect after removing the
+     * Account.
      */
     @RequestMapping(value = Routes.REMOVE_ACCOUNT, method = RequestMethod.POST)
     public ModelAndView removeAccount(@RequestParam("selectedAccountId") String accountId, UriComponentsBuilder builder) {
@@ -201,19 +230,26 @@ public class AdminController {
     }
 
     /**
-     * This method allows us to add a Transaction to MongoDB from the Admin Panel.
+     * This method allows us to add a Transaction to MongoDB from the Admin
+     * Panel.
      *
-     * @param senderAccountNumber the Account Number of the Sender in the Transaction
+     * @param senderAccountNumber the Account Number of the Sender in the
+     * Transaction
      * @param senderSortCode the Sort Code of the Sender in the Transaction
-     * @param recipientAccountNumber the Account Number of the Recipient in the Transaction
-     * @param recipientSortCode the Sort Code of the Recipient in the Transaction
+     * @param recipientAccountNumber the Account Number of the Recipient in the
+     * Transaction
+     * @param recipientSortCode the Sort Code of the Recipient in the
+     * Transaction
      * @param date the Date of the Transaction
      * @param value the monetary Value of the Transaction
      * @param accountNumber the Account Number that will own this Transaction
      * @param selectedTransactionType the Type of Transaction we want to create
-     * @param builder a Spring Framework component to map Uri Components to String Values
-     * @return the AdminPanel ModelAndView via a redirect after saving the new Customer.
-     * @throws ParseException when the date of the Transaction is entered in an incorrect format
+     * @param builder a Spring Framework component to map Uri Components to
+     * String Values
+     * @return the AdminPanel ModelAndView via a redirect after saving the new
+     * Customer.
+     * @throws ParseException when the date of the Transaction is entered in an
+     * incorrect format
      */
     @RequestMapping(value = Routes.ADD_TRANSACTION, method = RequestMethod.POST)
     public ModelAndView addTransaction(@RequestParam("senderAccountNumber") String senderAccountNumber,
@@ -225,12 +261,18 @@ public class AdminController {
             @RequestParam("accountNumber") String accountNumber,
             @RequestParam("selectedTransactionType") String selectedTransactionType,
             UriComponentsBuilder builder) throws ParseException {
-
+        
+        Transaction transaction = new Transaction();
+        
+        // Set the Transaction Date
         SimpleDateFormat formatter = new SimpleDateFormat("ddMMyyyy");
         Date transactionDate = formatter.parse(date);
+        transaction.setDate(transactionDate);
+        
+        //Set the Transaction Value
         Double transactionValue = Double.parseDouble(value);
-        Transaction transaction = new Transaction();
-
+        
+        //Set the Transaction Type
         switch (selectedTransactionType) {
             case "cash":
                 transaction.setTransactionType(TransactionType.CASH);
@@ -257,14 +299,12 @@ public class AdminController {
                 transaction.setTransactionType(TransactionType.OTHER);
                 break;
         }
-
-        transaction.setDate(transactionDate);
-
+        
+        // Build new Transaction
         transaction.setRecipientAccountNumber(recipientAccountNumber);
         transaction.setRecipientSortCode(recipientSortCode);
         transaction.setSenderAccountNumber(senderAccountNumber);
         transaction.setSenderSortCode(senderSortCode);
-
         transaction.setValue(transactionValue);
         transaction.setAccountNumber(accountNumber);
         transactionService.requestNewTransaction(transaction);
@@ -273,11 +313,14 @@ public class AdminController {
     }
 
     /**
-     * This method allows us to remove aa Transaction from MongoDB from the Admin Panel.
+     * This method allows us to remove aa Transaction from MongoDB from the
+     * Admin Panel.
      *
      * @param transactionId the ID of the Transaction we want to Remove
-     * @param builder a Spring Framework component to map Uri Components to String Values
-     * @return the AdminPanel ModelAndView via a redirect after removing the Account.
+     * @param builder a Spring Framework component to map Uri Components to
+     * String Values
+     * @return the AdminPanel ModelAndView via a redirect after removing the
+     * Account.
      */
     @RequestMapping(value = Routes.REMOVE_TRANSACTION, method = RequestMethod.POST)
     public ModelAndView removeTransaction(@RequestParam("selectedTransactionId") String transactionId, UriComponentsBuilder builder) {
