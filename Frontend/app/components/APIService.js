88e600a9d425d1b41a1cfdbc758fede212af22e4
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-12-14 11:40:02 +0000

    Updated API route to single point

--- Frontend/app/components/APIService.js
+++ Frontend/app/components/APIService.js
@@ -1,51 +1,52 @@
-angular.module('eBanking.APIService', ['ngResource'])
-  
-  .factory('eBankingAPIservice', ['$resource',
-    function($resource) {
+angular.module('eBanking.APIService', ['ngResource']).
+	
+	factory('eBankingAPIservice', ['$resource',
+		function($resource) {
 
-    var eBankingAPI = {};
+		var eBankingAPI = {};
+		eBankingAPI.route = 'https://localhost:8080/api';
 
-    eBankingAPI.getAccounts = function() {
-      return $resource('https://localhost:8080/api/customer/accounts');
-    }
+		eBankingAPI.getAccounts = function() {
+			return $resource(eBankingAPI.route + '/customer/accounts');
+		}
 
-    eBankingAPI.getAccount = function(accountId) {
-      return $resource('https://localhost:8080/api/customer/accounts/:accountId',
-        { accountId: accountId },
-        { query: { method: 'GET'} });
-    }
+		eBankingAPI.getAccount = function(accountId) {
+			return $resource(eBankingAPI.route + '/customer/accounts/:accountId',
+				{ accountId: accountId },
+				{ query: { method: 'GET'} });
+		}
 
-    eBankingAPI.getCustomerDetails = function() {
-      return $resource('https://localhost:8080/api/customer/');
-    }
+		eBankingAPI.getCustomerDetails = function() {
+			return $resource(eBankingAPI.route + '/customer/');
+		}
 
-    eBankingAPI.postCustomerDetails = function() {
-      return $resource('https://localhost:8080/api/customer/',
-        {},
-        { save: {
-            method: 'POST',
-            isArray: false,
-            headers:{'Content-Type':'application/json; charset=UTF-8'} 
-          }
-        });
-    }
+		eBankingAPI.postCustomerDetails = function() {
+			return $resource(eBankingAPI.route + '/customer/',
+				{},
+				{ save: {
+						method: 'POST',
+						isArray: false,
+						headers:{'Content-Type':'application/json; charset=UTF-8'} 
+					}
+				});
+		}
 
-    eBankingAPI.postTransfer = function(accountId) {
-      return $resource('https://localhost:8080/api/customer/accounts/:accountId/transactions',
-        { accountId: accountId },
-        { save: {
-            method: 'POST',
-            isArray: false,
-            headers:{'Content-Type':'application/json; charset=UTF-8'} 
-          }
-        });
-    }
+		eBankingAPI.postTransfer = function(accountId) {
+			return $resource(eBankingAPI.route + '/customer/accounts/:accountId/transactions',
+				{ accountId: accountId },
+				{ save: {
+						method: 'POST',
+						isArray: false,
+						headers:{'Content-Type':'application/json; charset=UTF-8'} 
+					}
+				});
+		}
 
-    eBankingAPI.getTransactions = function(accountId, page) {
-      return $resource('https://localhost:8080/api/customer/accounts/:accountId/transactions?page=:page',
-        { accountId: accountId, page: page },
-        { query: { method: 'GET', isArray: true} });
-    }
+		eBankingAPI.getTransactions = function(accountId, page) {
+			return $resource(eBankingAPI.route + '/customer/accounts/:accountId/transactions?page=:page',
+				{ accountId: accountId, page: page },
+				{ query: { method: 'GET', isArray: true} });
+		}
 
-    return eBankingAPI;
-  }]);
+		return eBankingAPI;
+}]);
