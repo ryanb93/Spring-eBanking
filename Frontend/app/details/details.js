d608e2f32e662b2ef6bb114af42b1776a52773df
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-12-16 11:22:27 +0000

    Details front end validation.

--- Frontend/app/details/details.js
+++ Frontend/app/details/details.js
@@ -16,19 +16,50 @@ angular.module('eBanking.detailsControllers', [])
         $scope.messages.details.err.err = false;
         $scope.messages.details.success.success = false;
 
+        $scope.displayDetailsError = function(message) {
+                $scope.messages.details.err.err = true;
+                $scope.messages.details.success.success = false;
+                $scope.messages.details.err.stat = "Details Update Failed";
+                $scope.messages.details.err.message = message;
+                return;
+        }
+
         $scope.processForm = function() {
+
+            //Front end validation on the sender account number existing.
+            if(!$scope.details.firstName) {
+                return $scope.displayDetailsError("You must enter a first name.");
+            }
+            if(!$scope.details.lastName) {
+                return $scope.displayDetailsError("You must enter a last name.");
+            }
+            if(!$scope.details.address.houseNumber) {
+                return $scope.displayDetailsError("You must enter a house number.");
+            }
+            if(!$scope.details.address.street) {
+                return $scope.displayDetailsError("You must enter a street.");
+            }
+            if(!$scope.details.address.postCode) {
+                return $scope.displayDetailsError("You must enter a post code.");
+            }
+            if(!$scope.details.address.city) {
+                return $scope.displayDetailsError("You must enter a city.");
+            }
+            if(!$scope.details.address.county) {
+                return $scope.displayDetailsError("You must enter a county.");
+            }
+            if(!$scope.details.address.country) {
+                return $scope.displayDetailsError("You must enter a country.");
+            }
+
             var test = eBankingAPIservice.postCustomerDetails().save($scope.details, function(result) {
+                $scope.messages.details.err.err = false;
                 $scope.messages.details.success.success = true;
                 $scope.messages.details.success.title = "Thank you";
                 $scope.messages.details.success.message = "Your details have been updated successfully."; 
             }, 
             function(data, status, headers, config) {
-                console.log(data);
-                console.log(data.message);
-                $scope.messages.details.err.err = true;
-                $scope.messages.details.success.success = false;
-                $scope.messages.details.err.title = "Update Failed";
-                $scope.messages.details.err.message = data.data.message;
+                $scope.displayDetailsError(data.data.message);
             });
 
         };
